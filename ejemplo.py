ac8cabb4f5bd1ca46e332c69beba82e073fc9c2e
alexisalvo <salvoalexis@yahoo.es>
2018-05-26 12:47:56 -0400

    primecommint

---
+++ ejemplo.py
@@ -0,0 +1,8 @@
+a = int(input("numero 1:"))
+b = int(input("numero 2:"))
+c = int(input("numero 3:"))
+d = int(input("numero 4:"))
+e = int(input("numero 5:"))
+suma = a+b+c+d+e
+resultado = "es la suma"
+print(suma, resultado)
---
+++ suma.py
@@ -0,0 +1,7 @@
+
+#! /usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+a = int(input("unidades :"))
+b = int(input("Precio :"))
+print("A pagar=", a*b)
